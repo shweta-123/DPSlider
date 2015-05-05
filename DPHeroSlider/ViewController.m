62359b647e95d4f99f025918dda3c9cd1860cace
Narendra Singh Sendhav <narendra.sendhav@diaspark.com>
2015-05-05 12:01:21 +0530

    same line

--- DPHeroSlider/ViewController.m
+++ DPHeroSlider/ViewController.m
@@ -18,7 +18,7 @@
 
 @implementation ViewController
 
-//Working Branch Same Line Commit"
+//Working Branch Same Line Commit Test"
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //@Method		: viewDidLoad
 //@Abstract		: We can initialize and setup the slider component in viewDidLoad
